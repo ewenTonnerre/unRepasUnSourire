fbd482f3cb204763882f923c38167f9bf0e2c2bc
Ewen TONNERRE <ewen.tonnerre18@gmail.com>
2022-02-19 17:54:13 +0100

    mise à jour participants + scroll reveal

---
+++ sr.js
@@ -0,0 +1,41 @@
+const sr = ScrollReveal();
+
+sr.reveal('.reveal',{
+    duration: 2000
+});
+
+sr.reveal('.reveal-left', {
+    origin: 'left',
+    distance: '50px',
+    duration: 1500
+});
+
+sr.reveal('.long-reveal-left', {
+    origin: 'left',
+    distance: '500px',
+    duration: 2000
+});
+
+sr.reveal('.reveal-right', {
+    origin: 'right',
+    distance: '50px',
+    duration: 1500
+});
+
+sr.reveal('.long-reveal-right', {
+    origin: 'right',
+    distance: '500px',
+    duration: 2000
+});
+
+sr.reveal('.reveal-top', {
+    origin: 'top',
+    distance: '50px',
+    duration: 1500
+});
+
+sr.reveal('.reveal-bottom', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1500
+});
